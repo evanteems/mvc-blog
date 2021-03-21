8a2cd0430e3a33c1d2aab28680ec6987047124f3
Evan Teems <evanteems40@yahoo.com>
2021-03-20 17:34:12 -0700

    server.js complete

---
+++ server.js
@@ -0,0 +1,41 @@
+// DEPENDENCIES START HERE!!!
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helperz = require('./utils/helperz');
+// Express start here!!!
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connections');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sesh = {
+    secret: 'Just to secretive',
+    cookie: {
+        expires: 7200000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sesh));
+
+const hbs = exphs.create({ helperz });
+// Apps start here
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false })/TouchEvent(() => {
+    app.listen(PORT, () => console.log('Beginning to Listen'));
+});
