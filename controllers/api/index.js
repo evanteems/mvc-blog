b10b70c11beed28f78ca3a1b3383ac694d70f102
Evan Teems <evanteems40@yahoo.com>
2021-03-20 19:27:18 -0700

    please finish user routes

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+// More dependencies inserted here!!!
+const router = require('express').Router();
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { User, Post, Comment, Vote } = require('../../models');
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password']},
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_text', 'created_at']
+            }
+        ]
+    })
+})
